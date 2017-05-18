8d91616de44b1b6d8f31db4d22033df5cfbd6f38
Jason Boser <jasonboser@gmail.com>
2017-05-18 15:47:23 -0500

    synchronized views

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,14 +7,14 @@ module.exports = function(app){
     app.post('/login',function(req,res){
         console.log(req.body);
         if(req.body.game == 'pacman'){
-            if (req.body.player == 'ghost'){
-                // play as ghost
-                res.render('ghost-player');
-            } else if (req.body.player == 'pacman') {
-                res.render('pacman-player');
-            } else {
+            // if (req.body.player == 'ghost'){
+            //     // play as ghost
+            //     res.render('ghost-player');
+            // } else if (req.body.player == 'pacman') {
+            //     res.render('pacman-player');
+            // } else {
                 res.render('board',req.body);
-            }
+            // }
 
         }else if(req.body.game == 'tetris'){
             res.render('tetris',req.body);
